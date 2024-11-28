7d6d32b0e4a63f1777822e97904d0001c5eab6a3
konstantin <kosta123139@gmail.com>
2024-11-28 22:29:18 +0300

    .

--- config/celery.py
+++ config/celery.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, unicode_literals
+
 import os
+
 from celery import Celery
 
 # Установка переменной окружения для настроек проекта
@@ -13,3 +15,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
 app.autodiscover_tasks()
+
+
+# Инициализируем Celery, чтобы работать с Django
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
