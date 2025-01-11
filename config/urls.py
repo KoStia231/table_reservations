554b9c9deb4cb3f61e4a4e49ac7d36731cf0c16a
konstantin <kosta123139@gmail.com>
2025-01-11 10:32:16 +0300

    .

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users'), name='users'),
     path('', include('main.urls', namespace='main'), name='main'),
     path('table-reservation/', include('table_rezerv.urls', namespace='table_rezerv'), name='table_rezerv'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
